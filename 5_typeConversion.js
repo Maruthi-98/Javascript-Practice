4e64800716ec8e64c3d772183e3a5753bbba0668
maruthi-98 <maruthialon316@gmail.com>
2022-04-11 14:36:26 +0530

    modified file names

---
+++ 5_typeConversion.js
@@ -0,0 +1,15 @@
+
+// type conversion in JavaScript
+
+let first_Name = "Maruthi";  // it is a string. Now lets convert it in to number
+let first_Name_Number = Number(first_Name);
+console.log( typeof first_Name_Number);  // typeof will help us find what type of data  it is and if we console.log(first_Name_Number)  it will return NaN because Maruthi is not a number
+
+
+let age = 25;  // it is a number. Now lets convert it in to string
+
+let age_string = String(age);  // number has been successfully converted using string()
+
+console.log(typeof age_string);  
+
+console.log(age_string);
