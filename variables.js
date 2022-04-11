749ae0350ba22190fe5286b7414c6e3812c93e32
maruthi-98 <maruthialon316@gmail.com>
2022-04-11 12:28:20 +0530

    Working with variables

---
+++ variables.js
@@ -0,0 +1,35 @@
+// Variables are containers for storing data (storing data values).
+
+// example 1:
+
+//In this example First_name, Last_name are variables, declared with the var keyword:
+
+var First_name = "G";
+
+var Last_name = "Maruthi"
+
+console.log(First_name);
+console.log(Last_name);
+
+var num1 = 10; // we can even store numbers and boolean values decimal values and many other values in variable
+
+console.log(num1);
+
+
+// example 2:
+
+//In this example Full_name are variables, declared with the let  keyword:
+
+let Full_name ="G Maruthi";
+
+console.log(Full_name);
+
+//example 3:
+// In this example PI are variables, declared with the const  keyword:
+// variable declared with const cannot be changed
+
+const PI = 3.14  
+
+//PI = 3.10  // trying to change the value in const variable but the results are ERROR - "Assignment to constant variable."
+
+console.log(PI);
