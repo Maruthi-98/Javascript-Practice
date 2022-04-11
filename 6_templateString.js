f828c24a1ff36b29d0d97aa530ecb0103702a4e4
maruthi-98 <maruthialon316@gmail.com>
2022-04-11 14:43:39 +0530

    template string

---
+++ 6_templateString.js
@@ -0,0 +1,5 @@
+let first_Name = "Maruthi";
+let age = 23;
+
+console.log(`My name is ${first_Name} and my age is ${age}`);  // `` are used to write a template string and a template string is a best way to concatenate a string 
+                                                               // we use ${} to deife a variable in a template string and we write the variable in {}
