aab4f1f253f39fbc0a8fd23f7e0c4939baab6b9b
maruthi-98 <maruthialon316@gmail.com>
2022-04-11 12:48:52 +0530

    String indexing

---
+++ stringIndexing.js
@@ -0,0 +1,16 @@
+// String Idexing in JavaScript 
+
+
+
+// In javascript there will be a postion or index of a characters
+
+// example: 
+
+let First_name = "Maruthi";
+
+//   M a r u t h i
+//   0 1 2 3 4 5 6
+
+console.log(First_name[0]);  // when we want to get  a characters from a string we use [] and then assign the index number into it;
+console.log(First_name[First_name.length-1]);  // length is a method which can be used to get the length of a string
+
