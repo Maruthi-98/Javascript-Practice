56e5b77f355a156b1c0d97d5e2eaf9cb343f14b4
Sai Kishore <maruthialone316@gmail.com>
2022-05-07 16:43:47 +0530

    working with function expressions in Javascript

---
+++ functionExpressions.js
@@ -0,0 +1,16 @@
+// working with function expression basics
+
+
+const addTwoNumbers = function(value1, value2){
+   return value1+value2;
+}
+
+console.log(addTwoNumbers(1,2));
+
+
+const addThreeNumbers = function(value1, value2, value3){
+   return value1+value2+value3;
+}
+
+const totalValue = addThreeNumbers(1,2,3);
+console.log(totalValue);
