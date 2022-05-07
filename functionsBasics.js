e87422e38c0964263b95a1056eceb0a9e7fc46e5
Sai Kishore <maruthialone316@gmail.com>
2022-05-07 16:35:04 +0530

    working with function basics

---
+++ functionsBasics.js
@@ -0,0 +1,22 @@
+// using functions in javascript
+
+// adding 2 numbers
+
+function addTwoNumbers(value1, value2){
+    return value1+value2;
+}
+
+const totalValue = addTwoNumbers(1,2);
+
+console.log(totalValue);
+
+
+// adding 3 numbers using function
+
+function addThreeNumbers(value1, value2, value3){
+    return value1+value2+value3;
+}
+
+const returnTotal = addThreeNumbers(1,2,3);
+console.log(returnTotal);
+
