4ebc80a202c907075ba3b5af1ed56af0801ff374
Sai Kishore <maruthialone316@gmail.com>
2022-05-10 13:19:02 +0530

    using arrow function in JS

---
+++ arrowFunctions.js
@@ -0,0 +1,24 @@
+// using arrow function in Javascript
+
+
+const arrowFunction = ()=>{
+    console.log("Hello this is a arrow function")
+}
+
+arrowFunction();
+
+// adding two numbers using arrow function
+
+const addingTwoNumbers = (value1, value2)=>{
+    return value1+value2;
+}
+
+console.log(addingTwoNumbers(1,2));
+
+// adding three numbers using arrow function
+
+const addingThreeNumbers = (value1,value2, value3)=>{
+    return value1+value2+value3;
+}
+
+console.log(1,2,3);
