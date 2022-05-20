6c0d4cb136326381621e1da4d9b72b951eb32c51
Sai Kishore <maruthialone316@gmail.com>
2022-05-20 20:31:56 +0530

    forEach method in javascript

---
+++ forEach.js
@@ -0,0 +1,11 @@
+// forEach in javascript
+
+const items = [
+    {name: "phone", price : 9999},
+    {name: "laptop", price : 29000},
+    {name : "Tv", price:12000}
+]
+
+items.forEach((item)=>{
+    console.log(item.name);
+})
